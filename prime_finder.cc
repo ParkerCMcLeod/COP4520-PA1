41cd13e1c4f35b756c378786c0a5b512a2f4d5f2
Parker McLeod <parkercmcleod@gmail.com>
2024-01-22 02:04:08 -0500

    completed single-threaded sieve of eratosthenes

--- prime_finder.cc
+++ prime_finder.cc
@@ -11,55 +11,63 @@ Due date: Friday, January 26th by 11:59 PM
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <algorithm>
 
-
-void save_to_file(std::chrono::milliseconds execution_time, int total_primes, long long sum_of_primes, std::vector<int>& ten_largest_primes_ascending) {
+void save_to_file(std::chrono::milliseconds execution_time, int total_primes, long long sum_of_primes, const std::vector<int>& ten_largest_primes_ascending) {
     std::ofstream outFile("primes.txt");
     if (outFile.is_open()) {
         outFile << execution_time.count() << " " << total_primes << " " << sum_of_primes << std::endl;
 
-        for(int i=0; i<ten_largest_primes_ascending.size(); i++)
-            outFile << ten_largest_primes_ascending[i] << " ";
-
-        outFile.close();
+    // we recorded the ten in descending order, so print backwards
+    for (int i = ten_largest_primes_ascending.size() - 1; i >= 0; i--) {
+        outFile << ten_largest_primes_ascending[i] << " ";
+    }
+        
+    outFile << std::endl;
+    outFile.close();
     } else {
         std::cerr << "Error opening primes.txt." << std::endl;
     }
 }
 
-
 int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        std::cerr << "Incorrect number of parameters passed. Usage is prime_finder [n]." << std::endl;
+        return 1; // exit
+    }
+
+    unsigned long long n = std::stoull(argv[1]);
+    if (n < 2) {
+        std::cerr << "No primes less than 2." << std::endl;
+        return 1;
+    }
 
     // timer start
     auto start = std::chrono::high_resolution_clock::now();
 
-    if(argc != 2)
-        std::cerr << "Incorrect number of parameters passed. Usage is prime_finder [n]." << std::endl;
-
-    unsigned long long n = std::atoi(argv[1]);
-
-    // create a bool array of size n assumed to be primes
-    std::vector<bool> A;
-    A.reserve(n);
-
-    int total_primes = {};
-    
-    unsigned long long sum_of_primes = {}; // guranteed to be at least 2^64 - 1 in C++11
-
+    std::vector<bool> A(n, true);
     std::vector<int> ten_largest_primes_ascending;
-    ten_largest_primes_ascending.reserve(10);
+    long long sum_of_primes = 0;
+    int total_primes = 0;
 
-    for(int i=2; i < std::sqrt(n); i++) {
-        int j = i * i;
-        while(j<=n) {
-            A[j] = false;
-            j++;
+    // sieve of eratosthenes
+    for (unsigned long long i = 2; i < n; ++i) {
+        if (A[i]) {
+            total_primes++;
+            sum_of_primes += i;
+
+            for (unsigned long long j = i * i; j < n; j += i) {
+                A[j] = false;
+            }
         }
     }
 
-    for(int i=2; i < n; i++)
-        if(A[i]) 
-            total_primes++;
+    // get the ten largest primes by traveling from top to bottom
+    for (unsigned long long i = n-1; i >= 2 && ten_largest_primes_ascending.size() < 10; i--) {
+        if (A[i]) {
+            ten_largest_primes_ascending.push_back(i);
+        }
+    }
 
     // timer end
     auto stop = std::chrono::high_resolution_clock::now();
@@ -67,6 +75,7 @@ int main(int argc, char *argv[]) {
     // timer calc
     auto execution_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
+    // save vars, formatted to spec
     save_to_file(execution_time, total_primes, sum_of_primes, ten_largest_primes_ascending);
 
     return 0;
