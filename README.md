8c8d69521b7887e01c5ffe9d4c0bf99d9ba0e43b
Parker McLeod <parkercmcleod@gmail.com>
2024-01-22 01:47:22 -0500

    blueprinting functionality of algo

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ Windows Compilation/Execution:
 
     Compilation: g++ -std=c++11 -pthread -o prime_finder prime_finder.cc
     Execution: prime_finder
-    Both: g++ -std=c++11 -pthread -o prime_finder prime_finder.cc && prime_finder
+    Both: g++ -std=c++11 -pthread -o prime_finder prime_finder.cc && prime_finder 100000000
 
 Sources:
 
-    https://www.geeksforgeeks.org/sieve-of-eratosthenes/
-    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#Segmented_sieve
+
--- prime_finder.cc
+++ prime_finder.cc
@@ -5,19 +5,18 @@ Instructor: Juan Parra
 Due date: Friday, January 26th by 11:59 PM
 */
 
+
+#include <chrono>
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <vector>
 
-// C++11 quirk only allows constexpr f(x)s to have a return statement in the body
-constexpr int pow(int base, int exp) {
-    return (exp == 0) ? 1 : base * pow(base, exp - 1);
-}
 
-void save_to_file(double execution_time, int total_primes, long long sum_of_primes, std::vector<int>& ten_largest_primes_ascending) {
+void save_to_file(std::chrono::milliseconds execution_time, int total_primes, long long sum_of_primes, std::vector<int>& ten_largest_primes_ascending) {
     std::ofstream outFile("primes.txt");
     if (outFile.is_open()) {
-        outFile << execution_time << " " << total_primes << " " << sum_of_primes << std::endl;
+        outFile << execution_time.count() << " " << total_primes << " " << sum_of_primes << std::endl;
 
         for(int i=0; i<ten_largest_primes_ascending.size(); i++)
             outFile << ten_largest_primes_ascending[i] << " ";
@@ -28,10 +27,20 @@ void save_to_file(double execution_time, int total_primes, long long sum_of_prim
     }
 }
 
-int main() {
-    constexpr int i = pow(10, 8); // compile-time calculation of 10^8
 
-    double execution_time = {};
+int main(int argc, char *argv[]) {
+
+    // timer start
+    auto start = std::chrono::high_resolution_clock::now();
+
+    if(argc != 2)
+        std::cerr << "Incorrect number of parameters passed. Usage is prime_finder [n]." << std::endl;
+
+    unsigned long long n = std::atoi(argv[1]);
+
+    // create a bool array of size n assumed to be primes
+    std::vector<bool> A;
+    A.reserve(n);
 
     int total_primes = {};
     
@@ -39,6 +48,24 @@ int main() {
 
     std::vector<int> ten_largest_primes_ascending;
     ten_largest_primes_ascending.reserve(10);
+
+    for(int i=2; i < std::sqrt(n); i++) {
+        int j = i * i;
+        while(j<=n) {
+            A[j] = false;
+            j++;
+        }
+    }
+
+    for(int i=2; i < n; i++)
+        if(A[i]) 
+            total_primes++;
+
+    // timer end
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    // timer calc
+    auto execution_time = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
 
     save_to_file(execution_time, total_primes, sum_of_primes, ten_largest_primes_ascending);
 
